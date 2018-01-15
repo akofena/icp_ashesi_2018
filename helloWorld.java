38e9bab7b74f3fca661fb509e0227455c469b702
David Ebo Adjepon-Yamoah <d.e.adjepon-yamoah@ncl.ac.uk>
2018-01-15 12:34:34 +0100

    helloWorld version 1
    This is the first commit to "helloWorld".

---
+++ helloWorld.java
@@ -0,0 +1,31 @@
+/**
+ * Week 1
+ */
+package icp_java_week1;
+
+/**
+ * @author dadjepon
+ *
+ */
+public class helloWorld {
+	
+		
+	public static void main(String a[]){
+	 	System.out.println("Hello Ashesi!");
+	 	
+//	 	int a1; 
+//	 	int b; 
+//	 	a1 = 32; 
+//	 	b = a1; 
+//	 	a1 = a1 + 1; 
+//	 	System.out.println(b); 
+	 	
+//	 	Person a; 
+//	 	Person b; 
+//	 	a = new Person("Everett"); 
+//	 	b = a; 
+//	 	a.changeName("Delmar"); 
+//	 	System.out.println(b.getName());
+
+	}
+}
